c998feaf753d5c1672f8a00ea8ef7d4fa9851ce0
Nyx <33086430+ninadarbaidze@users.noreply.github.com>
2023-08-01 11:31:31 +0400

    fix: catch end hour everytime

--- components/TimeFrameItem/TimeFrameItem.tsx
+++ components/TimeFrameItem/TimeFrameItem.tsx
@@ -5,16 +5,17 @@ const TimeFrameItem = (props) => {
   const [isMouseDown, setIsMouseDown] = useState(false);
 
   const triggerRef = useRef(null);
-  const listRef = useRef(null);
 
   const [initialHeight, setInitialHeigth] = useState(0);
 
   const handleMouseDown = (e) => {
     e.preventDefault();
-    setInitialHeigth(e.clientY);
+    setInitialHeigth(e.pageY);
     setIsMouseDown(true);
 
-    if (+e.target.id == props.hour) {
+    if (e.target.id === `${props.i}`) {
+      // Handle mouse down on the specific div (id={`hour-${props.i}`})
+      // Here, you can update state or do anything specific to this div.
       props.setTimeFrames((prev) => {
         const newState = [...prev];
         let obj = {
@@ -28,17 +29,17 @@ const TimeFrameItem = (props) => {
   };
 
   const handleMouseUp = useCallback(
-    (e) => {
+    (e, end) => {
+      // console.log('end', end);
       e.preventDefault();
       setIsMouseDown(false);
-      // console.log(listRef);
-      console.log(e.target.id);
-      if (+e.target.id == props.hour) {
+      // console.log(e.target.id);
+      if (end) {
         // Handle mouse up on the specific div (id={`hour-${props.i}`})
         // Here, you can update state or do anything specific to this div.
         props.setTimeFrames((prev) => {
           const newState = [...prev];
-          newState.at(-1).end = props.hour;
+          newState.at(-1).end = end;
           return newState;
         });
       }
@@ -56,13 +57,22 @@ const TimeFrameItem = (props) => {
       let yCord = 0;
       let initial = 0;
 
+      let end = 0;
+
       resizableElement!.style.left = '50px';
 
       const onMouseMoveBottomResize = (event) => {
+        if (!isMouseDown) {
+          // If isMouseDown is false, stop the execution of the function
+          return;
+        }
         event.preventDefault();
-        console.log('logloglog');
-        const dy = event.clientY - yCord;
-        if (initial < event.clientY) {
+        if (event.target.id) {
+          end = +event.target.id;
+        }
+        console.log('move');
+        const dy = event.pageY - yCord;
+        if (initial < event.pageY) {
           bottomHeight = 0;
 
           topHeight = topHeight + dy;
@@ -77,29 +87,21 @@ const TimeFrameItem = (props) => {
           resizableElement!.style.height = `${bottomHeight}px`;
         }
 
-        yCord = event.clientY;
+        yCord = event.pageY;
       };
 
+      // document.addEventListener('mousemove', onMouseMoveBottomResize);
       const onMouseUpBottomResize = (event) => {
         console.log('up');
-        handleMouseUp(event);
-        props.setTimeFrames((prev) => {
-          const newState = [...prev];
-          const endDateNotExists = newState.find(
-            (item) => item.start && !item.end
-          );
-          if (endDateNotExists) {
-            endDateNotExists.end = endDateNotExists.start + 1;
-          }
-          return newState;
-        });
+        setIsMouseDown(false);
+
+        handleMouseUp(event, end);
         document.removeEventListener('mousemove', onMouseMoveBottomResize);
       };
 
       const onMouseDownBottomResize = (event) => {
-        initial = event.clientY;
-        yCord = event.clientY;
-        console.log('down');
+        initial = event.pageY;
+        yCord = event.pageY;
 
         document.addEventListener('mousemove', onMouseMoveBottomResize);
         document.addEventListener('mouseup', onMouseUpBottomResize);
@@ -111,30 +113,26 @@ const TimeFrameItem = (props) => {
 
       return () => {
         resizeBottom?.removeEventListener('mousedown', onMouseDownBottomResize);
-        resizeBottom?.removeEventListener('mousemove', onMouseMoveBottomResize);
       };
     }
-  }, [handleMouseUp, initialHeight, isMouseDown, props]);
+  }, [handleMouseUp, initialHeight, isMouseDown]);
 
   return (
     <>
+      {isMouseDown && (
+        <div
+          className={`w-full min-w-[15px]  left-0  bg-red-500 z-50 border rounded-md overflow-x-clip overflow-y-auto absolute`}
+          ref={triggerRef}
+        ></div>
+      )}
       <li
         key={props.i}
-        className='flex gap-2'
-        ref={listRef}
-        id={props.hour}
+        className='flex gap-2 relative'
         onMouseDown={handleMouseDown}
         onMouseUp={handleMouseUp}
       >
-        {isMouseDown && (
-          <div
-            className={`w-full min-w-[15px]  left-0  bg-red-500 z-50 border rounded-md overflow-x-clip overflow-y-auto absolute`}
-            // style={{ top: initialHeight - 96 }}
-            ref={triggerRef}
-          ></div>
-        )}
         <div className='w-12 text-xs'>{props.hour}</div>
-        <div className='border w-full pb-12'>
+        <div id={`${props.i}`} className='border w-full pb-12'>
           {/* {timeFrames.at(-1)?.start == hour && (
       <p>{`${timeFrames.at(-1)?.start} - ${currentEndTime}`}</p>
     )} */}
